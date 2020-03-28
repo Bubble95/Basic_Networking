4ad0ab4fb86f0a7d556b752d21ea42c08b9f2890
Nelson Igbokwe <62732312+NelsonIg@users.noreply.github.com>
2020-03-28 22:47:02 +0100

    Add files via upload
    Client can now close the server by pressing a botton in the GUI.

--- networking.py
+++ networking.py
@@ -12,18 +12,17 @@ class Client:
         """
         self.adr     = adr
         self.port   = port
+        
+    #shutdown/ close server from client side
+    def close_server(self):
+        msg = 'shutdown'
         try: 
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.s.settimeout(5.0)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(10.0)
         except Exception as err:
             print(err) #print Exception to terminal
             return
-   
-    def get_file(self):
-        """
-            Receives a file from given server
-        """
-
+        
         # resolve hostname
         if type(self.adr) is not str: #raise exception if adr is not string
             raise Exception('adr must be of type String')    
@@ -38,34 +37,86 @@ class Client:
         elif self.port < 0 or self.port > 65535:
             raise Exception('Port must be between 0 and 65535')
         try:
-            self.s.connect((ip,self.port))
-            print('connected to: %s\n' %(ip))
+            s.connect((ip,self.port))
+            #print('connected to: %s\n' %(ip))
         except Exception as err:
             print(err)
-            self.s.close()
+            s.close()
             return
-        try:
-            msg = self.s.recv(4096).decode()
-                        
+        try:            
+            s.send(msg.encode())
         except Exception as err:
-            print('self.c.recv(size)\n', err)
-        self.s.close()
-        fileJson = json.loads(msg)
+            print('s.send(msg.encode())\n', err)
+            
+    def get_file(self):
+        """
+            Receives a file from given server
+        """
+        msg = 'file request'
+        try: 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(10.0)
+        except Exception as err:
+            print(err) #print Exception to terminal
+            return
+        
+        # resolve hostname
+        if type(self.adr) is not str: #raise exception if adr is not string
+            raise Exception('adr must be of type String')    
+        try:
+            ip = socket.gethostbyname(self.adr)
+        except Exception as err:
+            print('Exception using socket.gethostbyname():\n', err)
+            return
+        #try to connect to destination
+        if type(self.port) is not int: #raise exception if port is not int
+            raise Exception('ip must be of type integer')
+        elif self.port < 0 or self.port > 65535:
+            raise Exception('Port must be between 0 and 65535')
+        try:
+            s.connect((ip,self.port))
+            #print('connected to: %s\n' %(ip))
+        except Exception as err:
+            print(err)
+            s.close()
+            return
+        try:            
+            s.send(msg.encode())
+        except Exception as err:
+            print('s.send(msg.encode())\n', err)
+        try:   
+            msg = s.recv(4096).decode()                        
+        except Exception as err:
+            print('msg = s.recv(4096).decode()\n', err)
+        s.close()
+
+        try:
+            fileJson = json.loads(msg)
+        except Exception as err:
+            print('fileJson = json.loads(msg)',err)
         file = open(fileJson['name']+'copy.txt','w')
         file.write(fileJson['content'])
         file.close()
         return fileJson
+        #print(msg)
 
 class Server:
 
     def __init__(self,adr: str, port: int):
         self.port = port
         self.adr  = adr
-        try: 
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+    def close_server(self):
+        try:
+            self.s.close()
         except Exception as err:
-            print(err) #print Exception to terminal
+            print('self.s.close()',err)
             return
+    def close_connection(self):
+        try:
+            self.c.close()
+        except Exception as err:
+            print('self.s.close()',err)
 
     def launch(self):
         if type(self.adr) is not str: #raise exception if adr is not string
@@ -90,30 +141,77 @@ class Server:
 
     def send_file(self, fileName: str):
 
-        if type(fileName) is not str:
-            raise Exception('fileName must be String')
-        # load file
-        fileJson ={}
-        try:
-            file = open(fileName,'r')
+        #create socket
+        try: 
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except Exception as err:
-            print('file = open(fileName,\'r\'\n', err)
+            print(err) #print Exception to terminal
             return
-        fileJson['name'] = file.name
-        fileJson['content'] = file.read()
-        file.close()
-        msg = json.dumps(fileJson)
         
-        #accept connections
+        #Validate arguments
+        if type(self.adr) is not str: #raise exception if adr is not string
+            raise Exception('adr must be of type String')
+        if type(self.port) is not int: #raise exception if port is not int
+            raise Exception('ip must be of type integer')
+        elif self.port < 0 or self.port > 65535:
+            raise Exception('Port must be between 0 and 65535')
+        
+        #bind to adress and port
         try:
-            self.c, cAdr = self.s.accept()
-            print('Got connection from\n', cAdr)
+            s.bind((self.adr, self.port))
+            print("socket binded to %s" %(self.port))
         except Exception as err:
-            print('self.c, cAdr = self.s.accept()',err)
+            print('Exception cathed when executing self.s.bind((adr,port))\n',err)
             return
 
-        #send file
-        self.c.send(msg.encode())
+        # put the socket into listening mode - 5 scokets can be in queue, 6th is refused
+        try:
+            s.listen(5)
+            print("socket is listening\n")
+        except Exception as err:
+            print('self.s.listen(5)',err)
+            return
+
+        while True:
+            
+                # read file
+            if type(fileName) is not str:
+                raise Exception('fileName must be String')
+            # load file
+            fileJson ={}
+            try:
+                file = open(fileName,'r')
+            except Exception as err:
+                print('file = open(fileName,\'r\'\n', err)
+                return
+            fileJson['name'] = file.name
+            fileJson['content'] = file.read()
+            file.close()
+            msg = json.dumps(fileJson)
+            
+            #accept connections
+            try:
+                c, cAdr = s.accept()
+                print('Got connection from\n', cAdr)
+            except Exception as err:
+                print('self.c, cAdr = self.s.accept()',err)
+                return
+
+            #receive message
+            response = c.recv(4096).decode()
+            
+            if response == 'shutdown':
+                #shut down server from client side
+                s.close()
+                print('server closed by client:\n',cAdr)
+                break
+            else:
+                #send file and close connection if empty response
+                c.send(msg.encode())
+                c.close()
+            print(response)
+
+        
 
     def send(self, msg):
         try:
